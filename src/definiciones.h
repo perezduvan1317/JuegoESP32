00e7292b1b38b294b79c69383abbb2c0ac0605de
perezduvan1317 <112145280+perezduvan1317@users.noreply.github.com>
2025-05-02 21:23:10 -0500

    paso a paso

--- src/definiciones.h
+++ src/definiciones.h
@@ -2,6 +2,7 @@
 #include <Keypad.h>
 #include <Wire.h>
 #include <RTClib.h>
+#include <Stepper.h>
 
 #ifndef DEFINICIONES_H
 
@@ -9,20 +10,41 @@
 
 #define I2C_SDA 21
 #define I2C_SCL 22
-#define RXD2 16  // RX de esta ESP (no conectado si solo transmite)
-#define TXD2 17  // TX hacia la ESP principal
+#define RXD2 16 // RX de esta ESP (no conectado si solo transmite)
+#define TXD2 17 // TX hacia la ESP principal
 
+#define IN1 27
+#define IN2 26
+#define IN3 25
+#define IN4 33
+
+int pines[4] = {27, 26, 25, 33};  // IN1 a IN4
+
+int secuencia[8][4] = {
+  {1, 0, 0, 0},
+  {1, 1, 0, 0},
+  {0, 1, 0, 0},
+  {0, 1, 1, 0},
+  {0, 0, 1, 0},
+  {0, 0, 1, 1},
+  {0, 0, 0, 1},
+  {1, 0, 0, 1}
+};
+
+// Define el número de pasos por vuelta del 28BYJ-48
+const int pasosPorVuelta = 2048;
+
+Stepper motor(pasosPorVuelta, IN1, IN2, IN3, IN4);
 
 const byte NUM_FILAS = 4;
 const byte NUM_COLUMNAS = 4;
 
 // Definimos el mapa de teclas
 char teclas[NUM_FILAS][NUM_COLUMNAS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 
 // Pines que conectan las filas y columnas
 byte pinesFilas[NUM_FILAS] = {14, 5, 18, 19};
@@ -33,44 +55,3 @@ Keypad teclado = Keypad(makeKeymap(teclas), pinesFilas, pinesColumnas, NUM_FILAS
 RTC_DS3231 rtc;
 
 #endif
-/*
-
-
-
-
-
-
-
-#define pGreen      12 
-#define pWhite      27
-#define pYellow     32
-#define pBlue       04
-#define pRed        26
-
-#define lGreen      13 
-#define lWhite      14
-#define lYellow     33
-#define lBlue       15
-#define lRed        25
-
-volatile int contador = 0;
-
-hw_timer_t * timer = NULL;
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-
-#define LedGreenON      digitalWrite(lGreen, HIGH)
-#define LedGreenOFF     digitalWrite(lGreen, LOW)
-#define LedWhiteON      digitalWrite(lWhite, HIGH)
-#define LedWhiteOFF     digitalWrite(lWhite, LOW)
-#define LedYellowON     digitalWrite(lYellow, HIGH)
-#define LedYellowOFF    digitalWrite(lYellow, LOW)
-#define LedBlueON       digitalWrite(lBlue, HIGH)
-#define LedBlueOFF      digitalWrite(lBlue, LOW)
-#define LedRedON        digitalWrite(lRed, HIGH)
-#define LedRedOFF       digitalWrite(lRed, LOW)
-
-
-
-*/
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,16 @@
 String comando = "";
 
 void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
   Wire.begin();
   rtc.begin();
+  motor.setSpeed(10);
 
   //rtc.adjust(DateTime(2025, 4, 29, 20, 23, 0));  // Año, Mes, Día, Hora, Minuto, Segundo
 }
@@ -20,6 +26,7 @@ void procesarComando(String cmd) {
 }
 
 void loop() {
+  /*
   // Detectar teclas y enviarlas
   char tecla = teclado.getKey();
   if (tecla) {
@@ -39,4 +46,13 @@ void loop() {
       Serial.println(dia);
     }
   }
+
+*/
+  for (int paso = 0; paso < 8; paso++) {
+    for (int pin = 0; pin < 4; pin++) {
+      digitalWrite(pines[pin], secuencia[paso][pin]);
+    }
+    delay(2);  // Ajusta esto si va muy rápido o muy lento
+  }
+  delay(5000);
 }
