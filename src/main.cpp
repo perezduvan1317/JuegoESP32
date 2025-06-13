cdb93641198a5c0d1f8189941ab4cf872040a553
perezduvan1317 <112145280+perezduvan1317@users.noreply.github.com>
2025-06-13 18:02:39 -0500

    juego

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 
 String comando = "";
 
-void setup() {
+void setup()
+{
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
@@ -14,45 +15,169 @@ void setup() {
   rtc.begin();
   motor.setSpeed(10);
 
-  //rtc.adjust(DateTime(2025, 4, 29, 20, 23, 0));  // Año, Mes, Día, Hora, Minuto, Segundo
+  // rtc.adjust(DateTime(2025, 5, 2, 21, 27, 0));  // Año, Mes, Día, Hora, Minuto, Segundo
 }
 
 String HOLA = "GET_DATE";
 
-void procesarComando(String cmd) {
-  if (cmd == "GET_DATE") {
-    
+void procesarComando(String cmd)
+{
+  if (cmd == "GET_DATE")
+  {
   }
 }
 
-void loop() {
-  /*
+void loop()
+{
+
   // Detectar teclas y enviarlas
   char tecla = teclado.getKey();
-  if (tecla) {
+  if (tecla)
+  {
     Serial2.print("KEY:");
     Serial2.println(tecla);
     Serial.println(tecla);
-    //procesarComando(HOLA);
+    // procesarComando(HOLA);
   }
 
   // Escuchar comandos
-  while (Serial2.available()) {
+  while (Serial2.available())
+  {
     byte c = Serial2.read();
-    if (c == 0x7E) {
+    if (c == 0x7E)
+    {
       DateTime now = rtc.now();
       byte dia = now.day();
-      Serial2.write(dia);  // Enviar como byte binario
+      Serial2.write(dia); // Enviar como byte binario
       Serial.println(dia);
     }
-  }
-
-*/
-  for (int paso = 0; paso < 8; paso++) {
-    for (int pin = 0; pin < 4; pin++) {
-      digitalWrite(pines[pin], secuencia[paso][pin]);
+    if (c == 0x0A)
+    {
+      for (int paso = 0; paso < 400; paso++)
+      {
+        int fase = paso % 8; // Para repetir las 8 fases
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(pines[i], secuencia[fase][i]);
+        }
+        delay(2); // Puedes ajustar la velocidad (menor = más rápido)
+      }
+      c = 0x00;
+      Serial.println("moviendo paso a paso");
     }
-    delay(2);  // Ajusta esto si va muy rápido o muy lento
+
+
+    if (c == 0x01)
+    {
+      for (int paso = 0; paso < 463; paso++)
+      {
+        int fase = paso % 8; // Para repetir las 8 fases
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(pines[i], secuencia[fase][i]);
+        }
+        delay(2); // Puedes ajustar la velocidad (menor = más rápido)
+      }
+      c = 0x00;
+      Serial.println("moviendo paso a paso");
+    }
+    if (c == 0x02)
+    {
+      for (int paso = 0; paso < 463; paso++)
+      {
+        int fase = paso % 8; // Para repetir las 8 fases
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(pines[i], secuencia[fase][i]);
+        }
+        delay(2); // Puedes ajustar la velocidad (menor = más rápido)
+      }
+      c = 0x00;
+      Serial.println("moviendo paso a paso");
+    }
+    if (c == 0x03)
+    {
+      for (int paso = 0; paso < 463; paso++)
+      {
+        int fase = paso % 8; // Para repetir las 8 fases
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(pines[i], secuencia[fase][i]);
+        }
+        delay(2); // Puedes ajustar la velocidad (menor = más rápido)
+      }
+      c = 0x00;
+      Serial.println("moviendo paso a paso");
+    }
+    if (c == 0x04)
+    {
+      for (int paso = 0; paso < 463; paso++)
+      {
+        int fase = paso % 8; // Para repetir las 8 fases
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(pines[i], secuencia[fase][i]);
+        }
+        delay(2); // Puedes ajustar la velocidad (menor = más rápido)
+      }
+      c = 0x00;
+      Serial.println("moviendo paso a paso");
+    }
+    if (c == 0x05)
+    {
+      for (int paso = 0; paso < 463; paso++)
+      {
+        int fase = paso % 8; // Para repetir las 8 fases
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(pines[i], secuencia[fase][i]);
+        }
+        delay(2); // Puedes ajustar la velocidad (menor = más rápido)
+      }
+      c = 0x00;
+      Serial.println("moviendo paso a paso");
+    }
+    if (c == 0x06)
+    {
+      for (int paso = 0; paso < 440; paso++)
+      {
+        int fase = paso % 8; // Para repetir las 8 fases
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(pines[i], secuencia[fase][i]);
+        }
+        delay(2); // Puedes ajustar la velocidad (menor = más rápido)
+      }
+      c = 0x00;
+      Serial.println("moviendo paso a paso");
+    }
+    if (c == 0x07)
+    {
+      for (int paso = 0; paso < 463; paso++)
+      {
+        int fase = paso % 8; // Para repetir las 8 fases
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(pines[i], secuencia[fase][i]);
+        }
+        delay(2); // Puedes ajustar la velocidad (menor = más rápido)
+      }
+      c = 0x00;
+      Serial.println("moviendo paso a paso");
+    }
+    if (c == 0x08)
+    {
+      for (int paso = 0; paso < 463; paso++)
+      {
+        int fase = paso % 8; // Para repetir las 8 fases
+        for (int i = 0; i < 4; i++)
+        {
+          digitalWrite(pines[i], secuencia[fase][i]);
+        }
+        delay(2); // Puedes ajustar la velocidad (menor = más rápido)
+      }
+      c = 0x00;
+      Serial.println("moviendo paso a paso");
+    }
   }
-  delay(5000);
 }
